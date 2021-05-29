0c696a63532a889f60f41b298f2adfd6f2d66a20
ishida <webmaster@joe1cafe.com>
2021-05-30 01:10:15 +0800

    address layout comment @ 20210529

--- layuiHomeMall-v1.0.0/res/static/js/house.js
+++ layuiHomeMall-v1.0.0/res/static/js/house.js
@@ -41,7 +41,7 @@ layui.define(['element', 'carousel', 'table', 'util'], function(exports){
   $(window).on('resize', function(){
     var width = $(this).prop('innerWidth');
     ins1.reload({
-      height: (width > 768 ? 500 : 150) + 'px'
+      height: (width > 768 ? 81 : 60) + 'px'
     });
   });
   
@@ -66,13 +66,13 @@ layui.define(['element', 'carousel', 'table', 'util'], function(exports){
   
   //首页——点击切换
   $(".house-header").find("#switch").on('click', function(){
-    if(houseNav.hasClass("close")){
-      // $(".house-header").children(".layui-container")[0].style.height = 60 + houseNav[0].offsetHeight + 'px';
-      houseNav.removeClass("close");
-    }else{
-      // $(".house-header").children(".layui-container")[0].style.height = 50 + 'px';
-      houseNav.addClass("close");
-    }
+    // if(houseNav.hasClass("close")){
+    //   // $(".house-header").children(".layui-container")[0].style.height = 60 + houseNav[0].offsetHeight + 'px';
+    //   houseNav.removeClass("close");
+    // }else{
+    //   // $(".house-header").children(".layui-container")[0].style.height = 50 + 'px';
+    //   houseNav.addClass("close");
+    // }
   });
   
   //登入页——弹框
